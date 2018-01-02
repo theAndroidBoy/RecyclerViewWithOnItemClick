8ce7256b765f79c1d76454e2714a400c27869d6d
Asim <asimlatest27@gmail.com>
2018-01-02 17:25:29 +0500

    initial commit , working

---
+++ app/src/main/java/com/easyapps/recyclerviewwithonitemclick/RecyclerViewActivity.java
@@ -0,0 +1,51 @@
+package com.easyapps.recyclerviewwithonitemclick;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecyclerViewActivity extends Activity {
+
+    private List<Person> persons;
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.recyclerview_activity);
+
+        recyclerView=(RecyclerView)findViewById(R.id.rv);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(llm);
+        recyclerView.setHasFixedSize(true);
+
+        initializeData();
+        initializeAdapter();
+    }
+
+    private void initializeData(){
+        persons = new ArrayList<>();
+        persons.add(new Person("Emma Wilson", "23 years old", R.drawable.emma));
+        persons.add(new Person("Lavery Maiss", "25 years old", R.drawable.lavery));
+        persons.add(new Person("Lillie Watts", "35 years old", R.drawable.lillie));
+    }
+
+    private void initializeAdapter(){
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(persons, new CustomItemClickListener() {
+            @Override
+            public void onItemClick(View v, int position) {
+                Toast.makeText(RecyclerViewActivity.this,
+                        "its working ,Item Clicked: "+position,Toast.LENGTH_SHORT).show();
+            }
+        });
+        recyclerView.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/com/easyapps/recyclerviewwithonitemclick/RecyclerViewAdapter.java
@@ -0,0 +1,69 @@
+package com.easyapps.recyclerviewwithonitemclick;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.PersonViewHolder> {
+
+    CustomItemClickListener listener;
+
+    public static class PersonViewHolder extends RecyclerView.ViewHolder {
+
+        CardView cardView;
+        TextView personName;
+        TextView personAge;
+        ImageView personPhoto;
+
+        PersonViewHolder(View itemView) {
+            super(itemView);
+            cardView= (CardView)itemView.findViewById(R.id.cv);
+            personName = (TextView)itemView.findViewById(R.id.person_name);
+            personAge = (TextView)itemView.findViewById(R.id.person_age);
+            personPhoto = (ImageView)itemView.findViewById(R.id.person_photo);
+        }
+    }
+
+    List<Person> persons;
+
+    RecyclerViewAdapter(List<Person> persons, CustomItemClickListener listener){
+        this.persons = persons;
+        this.listener = listener;
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    @Override
+    public PersonViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item, viewGroup, false);
+        final PersonViewHolder pvh = new PersonViewHolder(v);
+        v.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onItemClick(v, pvh.getPosition());
+            }
+        });
+        return pvh;
+    }
+
+    @Override
+    public void onBindViewHolder(PersonViewHolder personViewHolder, int i) {
+        personViewHolder.personName.setText(persons.get(i).name);
+        personViewHolder.personAge.setText(persons.get(i).age);
+        personViewHolder.personPhoto.setImageResource(persons.get(i).photoId);
+    }
+
+    @Override
+    public int getItemCount() {
+        return persons.size();
+    }
+}
